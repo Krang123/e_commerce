df0776d2dd0e652f518204b8827ae01af766e8bd
Krang <108150590+Krang123@users.noreply.github.com>
2022-07-05 19:16:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const routes = require('./api_routes/routes');
 routes(app);
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.listen(port, () => {
 console.log(`Listening to port http://localhost:${port}`);
